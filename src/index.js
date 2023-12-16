f4aa432aeab1f1662bd5c150d85e34bcac1bf140
nmsoikat <nursaykat@gmail.com>
2023-12-16 21:36:29 +0600

    req interceptor and res interceptor for axios

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
+
+axios.interceptors.request.use((req) => {
+  console.log(req);
+  // req.headers.set('')
+  return req;
+})
+
+axios.interceptors.response.use((res) => {
+  return res;
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
